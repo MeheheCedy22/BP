c25796ace2a480f68e445d6fa92a7de16df68ea1
Marek Čederle <96355171+MeheheCedy22@users.noreply.github.com>
2025-05-11 22:30:01 +0200

    added word count to the script

--- scripts/count_with_spaces.ps1
+++ scripts/count_with_spaces.ps1
@@ -1,11 +1,11 @@
 # Array of file paths (replace with your actual file paths)
 $filePaths = @(
-    ".\content\chapter1-introduction.tex",
-    ".\content\chapter2-analysis.tex",
-    ".\content\chapter3-solution_proposal.tex",
-    ".\content\chapter4-experimenting.tex",
-    ".\content\chapter5-evaluation.tex",
-    ".\content\conclusion.tex"
+    ".\..\content\chapter1-introduction.tex",
+    ".\..\content\chapter2-analysis.tex",
+    ".\..\content\chapter3-solution_proposal.tex",
+    ".\..\content\chapter4-experimenting.tex",
+    ".\..\content\chapter5-evaluation.tex",
+    ".\..\content\conclusion.tex"
 )
 
 $OutputEncoding = [System.Text.Encoding]::UTF8
@@ -22,6 +22,7 @@ foreach ($filePath in $filePaths) {
         & pandoc $filePath -t plain --output=$tempFile --lua-filter=strip-math.lua
         $text = Get-Content $tempFile -Raw -Encoding UTF8
         $charCount = ($text | Measure-Object -Character).Characters
+	$wordCount = [math]::Round($charCount * (250 / 1800))
         Remove-Item $tempFile
         Write-Output $text
 
@@ -29,6 +30,7 @@ foreach ($filePath in $filePaths) {
         $fileCharCounts += [PSCustomObject]@{
             FileName = $filePath
             CharCount = $charCount
+	    WordCount = $wordCount
         }
 
         # Add to total character count
@@ -38,23 +40,24 @@ foreach ($filePath in $filePaths) {
         $fileCharCounts += [PSCustomObject]@{
             FileName = $filePath
             CharCount = 0
+	    WordCount = 0
         }
     }
 }
 
 # Print the header for the table
-Write-Host ("{0,-50} {1,15} {2,15}" -f "File Name", "Characters", "Percentage")
+Write-Host ("{0,-50} {1,15} {2,15} {3,15}" -f "File Name", "Characters", "Words", "Percentage")
 
-Write-Host ("-" * 82)
+Write-Host ("-" * 98)
 
 # Print each file's name, character count, and percentage
 $fileCharCounts | ForEach-Object {
     $percentage = if ($totalCharCount -gt 0) { [math]::Round(($_.CharCount / $totalCharCount) * 100, 2) } else { 0 }
-    Write-Host ("{0,-50} {1,15} {2,15}" -f $_.FileName, $_.CharCount, $percentage)
+    Write-Host ("{0,-50} {1,15} {2,15} {3,15}" -f $_.FileName, $_.CharCount, $_.WordCount, $percentage)
 }
 
 # Print a horizontal separator line
-Write-Host ("-" * 82)
+Write-Host ("-" * 98)
 
 # Print the total character count for all files (body only)
 Write-Host ("{0,-50} {1,15}" -f "Total", $totalCharCount) characters
@@ -70,3 +73,5 @@ $estimatedWords = [math]::Round($totalCharCount * (250 / 1800))
 
 # Print the estimated word count
 Write-Host ("{0,-50} {1,15}" -f "Estimated words", $estimatedWords) words
+
+
